3c0aaeee7fe8e2940bafabbe4a92a0b2e06e4a55
delta37 <delta37@delta37.delta37>
2018-01-24 06:05:38 +0300

    gaussian pyramid

---
+++ main.py
@@ -0,0 +1,33 @@
+import cv2
+import argparse
+from net import Net
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str)
+
+    args = parser.parse_args()
+    path = args.path
+    images = np.float32(cv2.imread(path, cv2.IMREAD_COLOR)[np.newaxis, :, :, :] / 255.0) 
+    print(images.shape)
+
+    net = Net()
+    res = net.predict(images)
+
+    print(res[0].shape)
+    print(res[1].shape)
+    print(res[2].shape)
+    print(res[3].shape)
+    print(res[4].shape)
+    plt.imshow(res[0][0])
+    plt.show()
+    plt.imshow(res[1][0])
+    plt.show()
+    plt.imshow(res[2][0])
+    plt.show()
+    plt.imshow(res[3][0])
+    plt.show()
+    plt.imshow(res[4][0])
+    plt.show()
--- net.py
+++ net.py
@@ -5,20 +5,34 @@ import scipy.ndimage.filters as fi
 
 class Net:
     def __init__(self):
-        pass
+        self.inputs = tf.placeholder(dtype=tf.float32, name="inputs")
+        self.gpyr = self.getGaussianPyramid(5)
+        self.outputs = self.gpyr(self.inputs)
     
     def getGaussianPyramid(self, N, size=5, sigma=3.0):
-        gaussian_kernel = tf.convert_to_tensor(generate_gaussian_kernel2d(self, size, sigma)) 
+        kernel = self.generate_gaussian_kernel2d(size, sigma) 
+        print(kernel)
+        gaussian_kernel = tf.convert_to_tensor(np.repeat(kernel, 3, axis=1).reshape((size, size, 3, 1)))
         def gaussianPyrDown(x):
             stacked = [x]
             for _ in range(N - 1):
-                x = tf.nn.conv2d(x, gaussian_kernel, strides=[1,1,1,1], padding='VALID')
+                x = tf.nn.depthwise_conv2d(x, gaussian_kernel, strides=[1,1,1,1], padding='VALID')[:,::2,::2,:]
                 stacked.append(x)
             return stacked
         return gaussianPyrDown
 
-     def generate_gaussian_kernel2d(self, size = 5, sigma = 3.0):
+    def generate_gaussian_kernel2d(self, size = 5, sigma = 3.0):
         inp = np.zeros((size, size))
         inp[size//2, size//2] = 1
-        return fi.gaussian_filter(inp, sigma)
-       
+        return np.float32(fi.gaussian_filter(inp, sigma))
+    
+    def predict(self, images):
+        init = tf.global_variables_initializer()
+        with tf.Session() as sess:
+            sess.run(init)
+            results = sess.run(self.outputs, feed_dict = {self.inputs: images})
+        return results
+
+
+
+
